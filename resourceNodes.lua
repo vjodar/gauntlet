1533487d7f835bd2032f27dd6f0072bdeee6d6e9
vjodar <vjodar@hawaii.edu>
2022-02-01 11:44:41 -1000

    Implemented vine, fungi, fishing hole particle systems

--- resourceNodes.lua
+++ resourceNodes.lua
@@ -64,13 +64,14 @@ function ResourceNodes:add_tree(_x,_y)
 
         --use sine wave funtion to syncronize shaking and particle emission
         --with hatchet hit
-        if (math.sin(self.state.harvestProgress*14)>0.9) then 
+        --only shake and emit particles if node is actively being harvested
+        if (math.sin(self.state.harvestProgress*14)>0.9) and self.state.beingHarvested then 
             if Player.state.facing=='right' then
                  self.spriteShake=1
             else --player is facing left
                 self.spriteShake=-1
             end
-            if self.state.beingHarvested then self.particles:emit(1) end 
+            self.particles:emit(1) --shoot out particles
         end
 
         --once depleted, change collision class to prevent further harvesting
@@ -185,14 +186,14 @@ function ResourceNodes:add_rock(_x,_y)
 
         --uses sine wave function to syncronize shaking and particle emission
         --with pickaxe swings
-        if (math.sin(self.state.harvestProgress*14)>0.9) then 
+        --only shake and emit particles if node is actively being harvested
+        if (math.sin(self.state.harvestProgress*14)>0.9) and self.state.beingHarvested then 
             if Player.state.facing=='right' then
                  self.spriteShake=1
             else --player is facing left
                 self.spriteShake=-1
             end
-            --shoot out particles
-            if self.state.beingHarvested then self.particles:emit(1) end 
+            self.particles:emit(1) --emit particles
         end
 
         --once depleted, change collision class to prevent further harvesting
@@ -257,8 +258,21 @@ function ResourceNodes:add_vine(_x,_y)
         self.collider:setCollisionClass('resourceNode')
         self.collider:setObject(self) --attach collider to this object
 
+        --sprites
         self.sprite=love.graphics.newImage('assets/vine.png')
         self.spriteDepleted=love.graphics.newImage('assets/vine_depleted.png')
+        self.spriteParticle=love.graphics.newImage('assets/vine_particle.png')
+
+        --particles
+        self.particles=love.graphics.newParticleSystem(self.spriteParticle,100)
+        self.particles:setParticleLifetime(0.5,1.5) --particles live 0.5s - 1.5s
+        self.particles:setLinearDamping(1) --'friction'
+        self.particles:setLinearAcceleration(0,20,0,50) --min and max acceleration
+        self.particles:setSpeed(0,20)
+        self.particles:setSpread(2*math.pi) --particles will spread 360degrees
+        self.particles:setEmissionArea('uniform',8,16) 
+        self.particles:setRotation(0,math.pi*2)
+        self.particles:setRelativeRotation(true) --particles will rotate as they speed up
 
         --node state metatable
         self.state={}
@@ -282,12 +296,15 @@ function ResourceNodes:add_vine(_x,_y)
             --spawn appropriate item, restart harvest progress
             Items:spawn_item(self.xPos,self.yPos,'vine_fiber')
             self.state.harvestProgress=0
+            self.particles:emit(10) --emit particles
 
             --after spawning item, reduce available resources by 1
             --update depleted state when resources are 0
             self.state.resources=self.state.resources-1
             if self.state.resources==0 then self.state.depleted=true end 
         end
+
+        self.particles:update(dt) --update particles
 
         --once depleted, change collision class to prevent further harvesting
         if self.state.depleted==true then 
@@ -306,14 +323,16 @@ function ResourceNodes:add_vine(_x,_y)
                 love.graphics.draw(
                     self.sprite,self.xPos,self.yPos,nil,
                     --animate by oscillating x,y stretching and y-offset
-                    1.1+math.sin(self.state.harvestProgressPrev*7+math.pi)*0.1,
-                    1.1+math.sin(self.state.harvestProgressPrev*7)*0.05,
-                    8,16-(1.1+math.sin(self.state.harvestProgressPrev*7))
+                    1.1+math.sin(self.state.harvestProgress*7+math.pi)*0.1,
+                    1.1+math.sin(self.state.harvestProgress*7)*0.05,
+                    8,16-(1.1+math.sin(self.state.harvestProgress*7))
                 )
             else
                 love.graphics.draw(self.sprite,self.xPos,self.yPos,nil,1,1,8,16)
             end
         end
+        --draw particles
+        love.graphics.draw(self.particles,self.xPos,self.yPos)
     end
 
     function node:harvestResource()
@@ -335,8 +354,21 @@ function ResourceNodes:add_fungi(_x,_y)
         self.collider:setCollisionClass('resourceNode')
         self.collider:setObject(self) --attach collider to this object
 
+        --sprites
         self.sprite=love.graphics.newImage('assets/fungi.png')
         self.spriteDepleted=love.graphics.newImage('assets/fungi_depleted.png')
+        self.spriteParticle=love.graphics.newImage('assets/fungi_particle.png')
+
+        --particles
+        self.particles=love.graphics.newParticleSystem(self.spriteParticle,100)
+        self.particles:setParticleLifetime(1,3) --particles live 1s - 3s
+        self.particles:setLinearDamping(4) --'friction'
+        self.particles:setSpeed(50,100) --min and max speed
+        self.particles:setDirection(4.7) --particles will shoot upward
+        self.particles:setSpread(4.7) --particles will spread 270degrees
+        self.particles:setEmissionArea('uniform',8,7) --particles will spawn throughout sprite
+        self.particles:setRotation(0,math.pi*2) --360 degree rotation
+        self.particles:setRelativeRotation(true) --particles will rotate as they speed up
 
         --node state metatable
         self.state={}
@@ -361,12 +393,15 @@ function ResourceNodes:add_fungi(_x,_y)
             --spawn appropriate item, restart harvest progress
             Items:spawn_item(self.xPos,self.yPos,'fungi_mushroom')
             self.state.harvestProgress=0
+            self.particles:emit(20) --emit particles
 
             --after spawning item, reduce available resources by 1
             --update depleted state when resources are 0
             self.state.resources=self.state.resources-1
             if self.state.resources==0 then self.state.depleted=true end 
         end
+
+        self.particles:update(dt) --update particles
 
         --once depleted, change collision class to prevent further harvesting
         if self.state.depleted==true then 
@@ -393,6 +428,8 @@ function ResourceNodes:add_fungi(_x,_y)
                 love.graphics.draw(self.sprite,self.xPos,self.yPos,nil,1,1,8,8)
             end
         end 
+        --draw particles
+        love.graphics.draw(self.particles,self.xPos,self.yPos)
     end
 
     --this function is called through world queries done by the player
@@ -426,6 +463,19 @@ function ResourceNodes:add_fishing_hole(_x,_y)
         self.animations.fishing_hole=anim8.newAnimation(self.grid('1-4',1), 0.15)
         self.animations.tool_harpoon=anim8.newAnimation(self.gridTool('1-20',1), 0.05)
         self.spriteDepleted=love.graphics.newImage('assets/fishing_hole_depleted.png')
+        self.spriteParticle=love.graphics.newImage('assets/fishing_hole_particle.png')
+
+        --particles
+        self.particles=love.graphics.newParticleSystem(self.spriteParticle,100)
+        self.particles:setParticleLifetime(0.4,0.8) --particles live 0.4s - 0.8s
+        self.particles:setLinearDamping(4) --'friction'
+        self.particles:setSpeed(50,100) --min and max speed
+        self.particles:setLinearAcceleration(-10,100,10,100) --min and max x,y acceleration
+        self.particles:setDirection(4.7) --particles will shoot upward
+        self.particles:setSpread(1.5) --particles will spread 90degrees
+        self.particles:setEmissionArea('uniform',4,4) --particles will spawn throughout sprite
+        self.particles:setRotation(0,math.pi*2) --360 degree rotation
+        self.particles:setRelativeRotation(true) --particles will rotate as they speed up
 
         --node state metatable
         self.state={}
@@ -462,6 +512,13 @@ function ResourceNodes:add_fishing_hole(_x,_y)
             if self.state.resources==0 then self.state.depleted=true end 
         end
 
+        --emit constant pulse of particles when node is being harvested
+        if self.state.beingHarvested and math.sin(self.state.harvestProgress*14)>0.9 then 
+            self.particles:emit(1) 
+        end 
+
+        self.particles:update(dt) --update particles
+
         --once depleted, change collision class to prevent further harvesting
         if self.state.depleted==true then 
             self.collider:setCollisionClass('depletedNode')
@@ -487,6 +544,8 @@ function ResourceNodes:add_fishing_hole(_x,_y)
                 )
             end
         end
+        --draw particles
+        love.graphics.draw(self.particles,self.xPos,self.yPos)
     end
 
     function node:harvestResource()
